0ad09d0d83aa1d02ec75890ff6908f2e586f6af6
Torben Fricke <torben.fricke@ial.uni-hannover.de>
2019-01-09 09:15:00 +0100

    added horizontal colorbar

--- tests/plot_colormap.py
+++ tests/plot_colormap.py
@@ -19,10 +19,18 @@ class PlotTest(unittest.TestCase):
 		bees = colormap.make_colormap([(0, 0, 0), (1, 1, 0)])
 		self.some_plot(bees)
 
+
 	def test_from_png(self):
 		fn = os.path.join(os.path.dirname(__file__), "opera.png")
 		cmap = colormap.colormap_from_png(fn)
 		self.some_plot(cmap)
+
+
+	def test_from_png_horizontal(self):
+		fn = os.path.join(os.path.dirname(__file__), "opera_horizontal.png")
+		cmap = colormap.colormap_from_png(fn)
+		self.some_plot(cmap)
+
 
 	def test_from_jpeg(self):
 		fn = os.path.join(os.path.dirname(__file__), "opera_jpg.png")
